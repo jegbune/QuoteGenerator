c674daa28a6f27b68504c05afffeacfe18ed16a6
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-08 02:00:04 +0100

    Quote Generator API machine

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const quote =fetch('https://type.fit/api/quotes')
-console.log(typeof quote)
+fetch('https://type.fit/api/quotes').then((response) => {
+    // console.log('resolved', response);
+    return response.json();
+}).then(data => {
+    console.log(data);
+
+}).catch((err) => {
+    // console.log('rejected', err)
+})
+//  console.log(data);
